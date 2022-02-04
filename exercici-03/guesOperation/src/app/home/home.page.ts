b055d3e75ac4b5f9d372fd7d9653fc89098d5bde
bcnitb <bcn4itb@gmail.com>
2022-02-04 16:02:48 +0100

    excercise 03

--- exercici-03/guesOperation/src/app/home/home.page.ts
+++ exercici-03/guesOperation/src/app/home/home.page.ts
@@ -10,10 +10,35 @@ export class HomePage {
   num:number;
   num1:number = this.randomNum(0,100);
   num2:number = this.randomNum(0,100);
+  sum:number;
+  
+  result:string;
+  resultArray:string[]=["Respuesta correcta", "¡Te has quedado corto!"];
+
+  check:boolean=false;
 
   constructor() {}
 
   randomNum(a, b){
     return Math.round(Math.random() * (b - a) + parseInt(a, 10));
+  }
+
+  checkAnswer(){
+    this.calculateSum();
+
+    if(this.sum == this.num){
+      this.result=this.resultArray[0];
+      this.check = true;
+    }
+    else if(this.sum < this.num){
+      this.result=this.resultArray[1];
+    }
+    else if(this.sum > this.num){
+      this.result=this.resultArray[2];
+    }
+  }
+
+  calculateSum() {
+    this.sum=this.num1+this.num2;
   }
 }
